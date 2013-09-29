508cddcbc5094f56df182ad6f2b7a19c5c829d4d
Mac McDell <macmcdell@gmail.com>
2013-09-28 18:01:17 -0700

    moved stuff to head of layout

--- PartyInvites/Controllers/MapController.cs
+++ PartyInvites/Controllers/MapController.cs
@@ -22,7 +22,22 @@ namespace PartyInvites.Controllers
 
         public ActionResult MultiInfo()
         {
+            ViewBag.Locations = Locations(); 
             return View("MultiInfo");
+           
+        }
+
+
+        private static string Locations()
+        {
+
+
+            return "['San Diego Childrems museum' ,32.710616,-117.165191,1], " +
+                   "['<h1 id=\"firstHeading\">Pinnacle Marina Tower</h1><div> <div style=\float:left\"> <img src=\"/content/images/museumTwr2.jpg\" alt=\"pinnacle tower\" /> </div>Situated in the ultimate Marina location overlooking the Bay, the 36 storey Pinnacle Museum Tower is a dramatic glass and solid concrete structure soaring 450 feet above the building’s Great Lobby. San Diego’s second tallest residential tower, The Pinnacle is a stone and marble architectural triumph filled with natural overhead light.</div>', 32.710072,-117.165121,2]";
+
+         
+
+
         }
 
     }
